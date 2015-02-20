cd8b0497ff1a45178f47665e6aff9586a5cbc66d
Amr Aboelela <amraboelela@gmail.com>
2015-02-20 11:51:26 -0800

    ImageView.m

--- UIKit/Images/ImagesView.m
+++ UIKit/Images/ImagesView.m
@@ -46,23 +46,23 @@ static CGImageRef png, jpeg, tiff;
     //if (nil == png) {
     CGDataProviderRef pngData = CGDataProviderCreateWithFilename("sunglass.png");
     //DLog(@"pngData: %@", pngData);
+    CGRect pngRect;
+    CGContextSaveGState(ctx);
+    CGContextScaleCTM(ctx, 1, -1.0);
+    CGContextTranslateCTM(ctx, 0, 300);
     if (pngData) {
         png = CGImageCreateWithPNGDataProvider(pngData, NULL, YES, kCGRenderingIntentDefault);
         //DLog(@"png: %@", png);
         CGDataProviderRelease(pngData);
+        pngRect = CGRectMake(150,150,130,130);//rect.size.width,rect.size.height);
         //}
         /*DLog(@"png: %@", png);
-        float width = 256;
-        float height = 256;
-        float xPosition = (rect.size.width - width)/2;
-        float yPosition = (rect.size.height - height)/2;
-        CGRect pngRect = CGRectMake(150,150,130,130);//rect.size.width,rect.size.height);
-        CGContextSaveGState(ctx);
-        //CGContextConcatCTM(ctx, CGContextGetUserSpaceToDeviceSpaceTransform(ctx));
-        CGContextTranslateCTM(ctx, 0, 300);
-        CGContextScaleCTM(ctx, 1, -1.0);*/
+        //float width = 256;
+        //float height = 256;
+        //float xPosition = (rect.size.width - width)/2;
+        //float yPosition = (rect.size.height - height)/2;
+        //CGContextConcatCTM(ctx, CGContextGetUserSpaceToDeviceSpaceTransform(ctx));*/
         CGContextDrawImage(ctx, pngRect, png);
-        //CGContextRestoreGState(ctx);
     }
 
     pngData = CGDataProviderCreateWithFilename("eartharrow.png");
@@ -80,7 +80,7 @@ static CGImageRef png, jpeg, tiff;
         pngRect = CGRectMake(70,70,100,100);
         CGContextDrawImage(ctx, pngRect, png);
     }
-    //CGContextRestoreGState(ctx);
+    CGContextRestoreGState(ctx);
  
     return;
     if (nil == jpeg) {
