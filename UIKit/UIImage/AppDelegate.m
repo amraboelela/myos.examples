8ed876944a86483384fbb2b8899d435678e95179
Amr Aboelela <amraboelela@gmail.com>
2015-02-27 17:28:50 -0800

    DLog();

--- UIKit/UIImage/AppDelegate.m
+++ UIKit/UIImage/AppDelegate.m
@@ -14,17 +14,25 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    DLog();
     CGRect frame = [[UIScreen mainScreen] bounds];
+    DLog();
     self.window = [[[UIWindow alloc] initWithFrame:frame] autorelease];
+    DLog();
     window.backgroundColor = [UIColor brownColor];
+    DLog();
     float width = kParentFrameSize;
-    float height = kParentFrameSize; 
+    DLog();
+    float height = kParentFrameSize;
+    DLog();
     float x = (frame.size.width - width) / 2.0;
     float y = (frame.size.height - height) / 2.0;
     MyImageView *imageView = [[[MyImageView alloc] initWithFrame:CGRectMake(x,y,width,height)] autorelease];
+    DLog();
     [window addSubview:imageView];
+    DLog();
     [window makeKeyAndVisible];
-    //DLog();
+    DLog();
     return YES;
 }
 
